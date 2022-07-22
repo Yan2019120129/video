19d63c633a241590214ecd93bb0ea6a689e0e2ee
Yan2019120129 <1556403682@qq.com>
2022-07-22 13:09:07 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+//引入Vue
 import Vue from 'vue'
+//引入App
 import App from './App.vue'
-
+//关闭Vue的生产提示
 Vue.config.productionTip = false
 
+//创建vm
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	el:'#app',
+	render: h => h(App),
+	mounted(){
+		setTimeout(()=>{
+			this.$destroy()
+		},5000)
+		
+	}
+})
